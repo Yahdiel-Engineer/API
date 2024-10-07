e0b93d029fe8e6a834bb9cdccb29be1d057f0bef
Alaska36 <bkoyaweda1@gmail.com>
2024-10-07 06:57:13 +0100

    message

--- API/create_tables.py
+++ API/create_tables.py
@@ -1,6 +1,12 @@
 # create_tables.py
 
-from config import create_tables
+from API.config import create_tables
+import sys
+import os
+
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 if __name__ == "__main__":
     create_tables()
