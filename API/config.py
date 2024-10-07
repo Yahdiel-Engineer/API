54d621241b517da441773bf5da4aa711d7c87d1b
Alaska36 <bkoyaweda1@gmail.com>
2024-10-07 04:05:07 +0100

    Décris ici les modifications apportées

--- API/config.py
+++ API/config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql://postgres:0000@localhost:5432/test'
+DATABASE_URL = "postgresql://postgres.ezdgfffkfljicnoozrcl:Alaska40023010+*@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush = False, bind=engine)
